bc2b3fbe81cc5c3b3a797b1f2ae7b3339b7ba1cf
MihailDenisov <Mihan.denisov@gmail.com>
2025-03-28 14:03:25 +0300

    Update info.c

--- source/daplink/info.c
+++ source/daplink/info.c
@@ -21,13 +21,13 @@
 
 #include <string.h>
 #include "info.h"
-#include "target_config.h"
+//#include "target_config.h"
 #include "read_uid.h"
 #include "util.h"
 #include "crc.h"
 #include "daplink.h"
 #include "settings.h"
-#include "target_board.h"
+//#include "target_board.h"
 #include "flash_hal.h"
 
 static char hex_to_ascii(uint8_t x)
@@ -66,20 +66,20 @@ const char *info_get_unique_id(void)
     return string_unique_id;
 }
 
-const char *info_get_board_id(void)
-{
-    return string_board_id;
-}
+//const char *info_get_board_id(void)
+//{
+//    return string_board_id;
+//}
 
 const char *info_get_host_id(void)
 {
     return string_host_id;
 }
 
-const char *info_get_target_id(void)
-{
-    return string_target_id;
-}
+//const char *info_get_target_id(void)
+//{
+//    return string_target_id;
+//}
 
 const char *info_get_hic_id(void)
 {
@@ -104,11 +104,11 @@ static void setup_basics(void)
 {
     uint8_t i = 0, idx = 0;
     uint16_t family_id = get_family_id();
-    memset(string_board_id, 0, sizeof(string_board_id));
+//    memset(string_board_id, 0, sizeof(string_board_id));
     memset(string_host_id, 0, sizeof(string_host_id));
-    memset(string_target_id, 0, sizeof(string_target_id));
+//    memset(string_target_id, 0, sizeof(string_target_id));
     memset(string_hic_id, 0, sizeof(string_hic_id));
-    memset(string_board_id, 0, sizeof(string_board_id));
+//    memset(string_board_id, 0, sizeof(string_board_id));
     // Host ID
     idx = 0;
 
@@ -118,13 +118,13 @@ static void setup_basics(void)
 
     string_host_id[idx++] = 0;
     // Target ID
-    idx = 0;
-
-    for (i = 0; i < 4; i++) {
-        idx += util_write_hex32(string_target_id + idx, target_id[i]);
-    }
-
-    string_target_id[idx++] = 0;
+//    idx = 0;
+//
+//    for (i = 0; i < 4; i++) {
+//        idx += util_write_hex32(string_target_id + idx, target_id[i]);
+//    }
+//
+//    string_target_id[idx++] = 0;
     // HIC ID
     idx = 0;
     idx += util_write_hex32(string_hic_id + idx, hic_id);
@@ -163,8 +163,8 @@ static void setup_basics(void)
 static void setup_unique_id()
 {
     memset(string_unique_id, 0, sizeof(string_unique_id));
-    strcat(string_unique_id, string_board_id);
-    strcat(string_unique_id, string_family_id);
+//    strcat(string_unique_id, string_board_id);
+//    strcat(string_unique_id, string_family_id);
     strcat(string_unique_id, string_host_id);
     strcat(string_unique_id, string_hic_id);
 }
@@ -194,18 +194,18 @@ void info_init(void)
     setup_string_descriptor();
 }
 
-void info_set_uuid_target(uint32_t *uuid_data)
-{
-    uint32_t idx = 0;
-    // Save the target ID
-    memcpy(target_id, uuid_data, 16);
-    // patch for MAC use. Make sure MSB bits are set correctly
-    uuid_data[2] |= (0x2 << 8);
-    uuid_data[2] &= ~(0x1 << 8);
-    idx += util_write_hex16(string_mac + idx, uuid_data[2] & 0xFFFF);
-    idx += util_write_hex32(string_mac + idx, uuid_data[3]);
-    string_mac[idx++] = 0;
-}
+//void info_set_uuid_target(uint32_t *uuid_data)
+//{
+//    uint32_t idx = 0;
+//    // Save the target ID
+//    memcpy(target_id, uuid_data, 16);
+//    // patch for MAC use. Make sure MSB bits are set correctly
+//    uuid_data[2] |= (0x2 << 8);
+//    uuid_data[2] &= ~(0x1 << 8);
+//    idx += util_write_hex16(string_mac + idx, uuid_data[2] & 0xFFFF);
+//    idx += util_write_hex32(string_mac + idx, uuid_data[3]);
+//    string_mac[idx++] = 0;
+//}
 
 bool info_get_bootloader_present(void)
 {
